35fa8bfac403a4169b6c1cfaeef7e01a76dc32fa
Chao <cch@docrud.com>
2018-12-20 13:44:18 +0800

    fix: demo 文件更详细

--- demo.php
+++ demo.php
@@ -1,8 +1,19 @@
 <?php
-
+// 引入文件
 include_once('MysqlToDoc.php');
 
 // 实例化
-$mtd = new MysqlToDoc('127.0.0.1', 'dbname', 'username', 'password');
+$mtd = new MysqlToDoc('127.0.0.1', 'tob', 'root', '');
+
+// 模板
+$templates = [
+    'table' => "{tableName} {tableComment} \r\n{columns}",
+    'column' => "{field} {type} {collation} {nullName} {keyName} {default} {extra} {comment} ",
+];
+
 // 执行
-$doc = $mtd->run();
+$doc = $mtd->run($templates);
+
+?>
+
+<textarea style="width: 700px; height: 350px;"><?= $doc ?></textarea>
